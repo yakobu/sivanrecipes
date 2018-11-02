8d405988007342cff4bdfc5178101bf1cca1f108
ronen-y <ronenya4321@gmail.com>
2018-11-02 12:11:20 +0200

    fix whatsapp

--- client/src/components/UI/Card/Card.js
+++ client/src/components/UI/Card/Card.js
@@ -97,7 +97,7 @@ const RecipeReviewCard = (props) => {
                                 </EmailShareButton>
                             </div>
                             <div className={classes.shareButton}>
-                                <WhatsappShareButton url={window.location.href}
+                                <WhatsappShareButton url={props.body}
                                                      title={props.title}
                                                      separator={"\n"}>
                                     <WhatsappIcon size={32} round={true}/>
