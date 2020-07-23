23a0fa5c4a93a131a05773dbee5a54284bd5a0de
yakobu <ronenya4321@gmail.com>
2020-07-23 19:05:41 +0300

    Remove things

--- client/src/components/UI/ContactInfo/ContactInfo.js
+++ client/src/components/UI/ContactInfo/ContactInfo.js
@@ -6,10 +6,6 @@ import logo from '../../../assets/Chef_book.svg'
 const contact_info = (props) => (
     <div className={Classes.ContactInfo}>
         <h3>{props.header}</h3>
-        <ul className={Classes.SivanInfo}>
-            <li>sivan.rnv@gmail.com</li>
-            <li>0533010395</li>
-        </ul>
         <img src={logo} className={Classes.AppLogo} alt="logo" />
     </div>
 );
